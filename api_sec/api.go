d663f8a09019d986a589dfc12134e49efcb0a3c1
Eden Zehavy <eden.zehavy@post.runi.ac.il>
2024-11-25 13:18:45 +0200

    Fixed bugs in balance handler

--- api_sec/api.go
+++ api_sec/api.go
@@ -4,12 +4,12 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"bytes"
+	"io"
+	"log"
 	"strconv"
 	"strings"
 	"time"
-
-	"bytes"
-	"io"
 
 	"github.com/dgrijalva/jwt-go"
 )
@@ -121,6 +121,9 @@ func Login(w http.ResponseWriter, r *http.Request) {
 }
 
 func AccountsHandler(w http.ResponseWriter, r *http.Request, claims *Claims) {
+	r.Body = http.MaxBytesReader(w, r.Body, 1048576) //Limit request body to 1MB
+	defer r.Body.Close()
+
 	//Only admins can send accounts requests
 	if claims.Role != "admin" {
 		http.Error(w, "Unauthorized", http.StatusForbidden)
@@ -181,25 +184,42 @@ func listAccounts(w http.ResponseWriter, r *http.Request, claims *Claims) {
 }
 
 func BalanceHandler(w http.ResponseWriter, r *http.Request, claims *Claims) {
+	log.Println("Reached Balance Handler")
 	r.Body = http.MaxBytesReader(w, r.Body, 1048576) //Limit request body to 1MB
 	defer r.Body.Close()
 
 	switch r.Method {
 	case http.MethodGet:
 		getBalance(w, r, claims)
-	//Makes sure only users can modify the balance
-	case http.MethodPost:
-		if claims.Role == "user" {
+
+	//Makes sure all roles can only modify their own balance
+	//users passed this check in Auth but this one is for admin accounts
+	//wanting to deposit or withdraw from their account. they cannot do this to other users accounts.
+	case http.MethodPost, http.MethodDelete:
+		// POST and DELETE methods need request body
+		r.Body = http.MaxBytesReader(w, r.Body, 1048576) // Limit request body to 1MB
+		defer r.Body.Close()
+
+		var acc Account
+		if err := json.NewDecoder(r.Body).Decode(&acc); err != nil {
+			log.Println("Error decoding in balance handler")
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if claims.UserID != acc.UserID {
+			http.Error(w, "Unauthorized", http.StatusForbidden)
+			return
+		}
+
+		if r.Method == http.MethodPost {
 			depositBalance(w, r, claims)
-			return
-		}
-		http.Error(w, "Unauthorized", http.StatusForbidden)
-	case http.MethodDelete:
-		if claims.Role == "user" {
+		} else {
 			withdrawBalance(w, r, claims)
-			return
 		}
-		http.Error(w, "Unauthorized", http.StatusForbidden)
+
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
@@ -298,6 +318,7 @@ func withdrawBalance(w http.ResponseWriter, r *http.Request, claims *Claims) {
 
 // Middleware for checking that the body of the request is of type JSON
 func ContentTypeJSON(next http.HandlerFunc) http.HandlerFunc {
+	log.Println("Reached JSON middleware")
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("Content-Type") != "application/json" {
 			http.Error(w, "Content-Type must be application/json", http.StatusUnsupportedMediaType)
@@ -308,18 +329,22 @@ func ContentTypeJSON(next http.HandlerFunc) http.HandlerFunc {
 }
 
 func Auth(next func(http.ResponseWriter, *http.Request, *Claims)) http.HandlerFunc {
+	log.Println("Reached Auth middleware")
 	return func(w http.ResponseWriter, r *http.Request) {
 		tokenStr := r.Header.Get("Authorization")
 		if tokenStr == "" {
+			log.Println("1")
 			http.Error(w, "Missing token", http.StatusUnauthorized)
 			return
 		}
 		tokenStr = strings.TrimPrefix(tokenStr, "Bearer ")
 		claims := &Claims{}
 		token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+			log.Println("2")
 			return jwtKey, nil
 		})
 		if err != nil || !token.Valid {
+			log.Println("3")
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
@@ -333,6 +358,7 @@ func Auth(next func(http.ResponseWriter, *http.Request, *Claims)) http.HandlerFu
 		//an unauthorized error is returned.
 
 		if claims.Role != "admin" {
+			log.Println("4")
 			userIDParam := r.URL.Query().Get("user_id")
 			if userIDParam != "" {
 				userID, err := strconv.Atoi(userIDParam)
@@ -370,6 +396,8 @@ func Auth(next func(http.ResponseWriter, *http.Request, *Claims)) http.HandlerFu
 			}
 		}
 
+		log.Println("called next function from auth")
 		next(w, r, claims)
+
 	}
 }
--- api_sec/log.go
+++ api_sec/log.go
@@ -9,7 +9,7 @@ import (
 	"strconv"
 )
 
-type Log struct {
+type LogData struct {
 	Req struct {
 		URL        string `json:"url"`
 		QSParams   string `json:"qs_params"`
@@ -22,32 +22,32 @@ type Log struct {
 	} `json:"rsp"`
 }
 
-// logRequest logs the request and response
-func createLog(w http.ResponseWriter, r *http.Request, statusCode int, bodyLen int) {
+//logRequest logs the request and response
+func createLog(r *http.Request, statusCode int, bodyLen int) {
 	reqURL := r.URL.String()
 	qParams := r.URL.Query().Encode()
 	headers := fmt.Sprintf("%v", r.Header)
 	reqBodyLen := int(r.ContentLength)
 
-	currLog := Log{}
+	currLog := LogData{}
 	currLog.Req.URL = reqURL
 	currLog.Req.QSParams = qParams
 	currLog.Req.Headers = headers
 	currLog.Req.ReqBodyLen = reqBodyLen
 
-	//Compute status class
+	//compute status class
 	statusClass := strconv.Itoa(statusCode/100) + "xx"
 	currLog.Rsp.StatusClass = statusClass
 	currLog.Rsp.RspBodyLen = bodyLen
 
-	//Marshal the current log into json format
+	//marshal the current log into json format
 	logData, err := json.Marshal(currLog)
 	if err != nil {
 		log.Println("Can not marshal log", err)
 		return
 	}
 
-	//Create access.log file
+	//create access.log file
 	file, err := os.OpenFile("access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Println("Can not open log file", err)
@@ -55,7 +55,7 @@ func createLog(w http.ResponseWriter, r *http.Request, statusCode int, bodyLen i
 	}
 	defer file.Close()
 
-	//Write the log to the file
+	//write the log to the file
 	_, err = file.Write(append(logData, '\n', '\n'))
 	if err != nil {
 		log.Println("Error writing log:", err)
@@ -64,27 +64,27 @@ func createLog(w http.ResponseWriter, r *http.Request, statusCode int, bodyLen i
 
 func LogMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		//Capture the original ResponseWriter to count the response size
+		//capture the original ResponseWriter to count the response size and status code
 		recorder := &ResponseRecorder{ResponseWriter: w, StatusCode: http.StatusOK}
 		next(recorder, r)
-		createLog(recorder, r, recorder.StatusCode, recorder.BodyLen)
+		createLog(r, recorder.StatusCode, recorder.BodyLen)
 	}
 }
 
-// wraps the http.ResponseWriter to allow acess to the response status and body length
+//wraps the http.ResponseWriter to allow acess to the response status and body length
 type ResponseRecorder struct {
 	http.ResponseWriter
 	StatusCode int
 	BodyLen    int
 }
 
-// overrides ResponseWriter WriteHeader, captures the status code for the response
+//overrides ResponseWriter WriteHeader, captures the status code for the response
 func (rec *ResponseRecorder) WriteHeader(statusCode int) {
 	rec.StatusCode = statusCode
 	rec.ResponseWriter.WriteHeader(statusCode)
 }
 
-// overrides ResponseWriter Write, captures the status code for the response
+//overrides ResponseWriter Write, captures the status code for the response
 func (rec *ResponseRecorder) Write(rspBody []byte) (n int, err error) {
 	n, err = rec.ResponseWriter.Write(rspBody)
 	rec.BodyLen += n
